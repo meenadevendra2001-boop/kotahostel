54f0fab29cdb20a16fe6f0afa9725877e6bf4bb3
meenadevendra2001-boop <meenadevendra2001@gmail.com>
2026-07-10 01:10:39 +0530

    Update script.js with Firestore

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ const firebaseConfig = {
   projectId: "kotahostel-23d4d",
   storageBucket: "kotahostel-23d4d.firebasestorage.app",
   messagingSenderId: "618520036441",
-  appId: "1:618520036441:web:deb85bb9ec148f44381d48"
+  appId: "1:618520036441:web:deb85bb9ec148f44381d48",
+  measurementId: "G-PFXFZBG947"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
+const db = firebase.firestore();
 
 function register() {
+  const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      return db.collection("users").doc(userCredential.user.uid).set({
+        name: name,
+        email: email,
+        phone: "",
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    })
     .then(() => {
       document.getElementById("message").innerText = "Registration Successful!";
     })
@@ -32,7 +43,7 @@ function login() {
     .then(() => {
       document.getElementById("message").innerText = "Login Successful!";
     })
-    .catch((error) => {
+    .catch(error => {
       document.getElementById("message").innerText = error.message;
     });
 }
